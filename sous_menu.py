dff3b4fb031b7126143cfee26be1776c0a6bc4b3
Alf <101100549+AlfMortorsport@users.noreply.github.com>
2023-06-01 10:09:24 +0200

    motif sous menu

--- sous_menu.py
+++ sous_menu.py
@@ -1,10 +1,16 @@
 import tkinter as tk
 from tkinter import messagebox
+from datetime import datetime
+
+def refresh_page():
+    root.update()  # Update the root window
+    root.after(1000, refresh_page)  # Schedule the refresh_page function to be called after 1 second
 
 def show_barrier_dialog():
     value = barrier_entry.get()
     if value.isdigit() and 4 <= int(value) <= 40:
         messagebox.showinfo("Nombre de barrières", f"Vous avez choisi {value} barrières.")
+        start_timer()
     else:
         messagebox.showerror("Erreur", "Veuillez entrer un nombre valide entre 4 et 40.")
 
@@ -15,74 +21,63 @@ def create_buttons_frame(root, texts, commands):
         button.pack(side=tk.LEFT, padx=5, pady=5)
     frame.pack()
 
+value_joueur = 0
+value_taille_tableau = 0
 
-def on_5x5_click():
-    value = 5
-    return value
+def on_button_click(value):
+    global value_joueur, value_taille_tableau
+    if value in [5, 7, 9, 11]:
+        value_taille_tableau = value
+    elif value in [2, 4]:
+        value_joueur = value
 
+def reset_game():
+    barrier_entry.delete(0, tk.END)
 
-def on_7x7_click():
-    value = 7
-    return value
+def start_timer():
+    global start_time
+    start_time = datetime.now()
+    print("Chronomètre démarré.")
+    update_timer()
 
-def on_9x9_click():
-    value = 9
-    return value
-
-def on_11x11_click():
-    value = 11
-    return value
-
-def on_2_players_click():
-    value = 2
-    return value
-
-def on_4_players_click():
-    value = 4
-    return value
-
-
+def update_timer():
+    elapsed_time = datetime.now() - start_time
+    timer_label.configure(text=str(elapsed_time))
+    timer_label.after(1000, update_timer)  # Update the timer every second (1000 milliseconds)
 
 root = tk.Tk()
 root.title("Quoridor Dame")
-
-root.geometry("800x800")  # Définir la taille de la fenêtre (largeur x hauteur)
+root.geometry("800x800")
 
 barrier_label = tk.Label(root, text="Choisissez taille de tableau:")
 barrier_label.pack(pady=5)
 
-def yorel():
-    if evl[0]=="5x5":
-      print(5)
-      valeur=5
-      print(valeur)
-    return valeur
-
-evl=["5x5", "7x7", "9x9", "11x11"]
-
-
-# Création des boutons 5x5, 7x7, 9x9 et 11x11
 button_frame1 = tk.Frame(root)
-create_buttons_frame(button_frame1, [evl],[yorel])
+create_buttons_frame(button_frame1, ["5x5", "7x7", "9x9", "11x11"], [lambda: on_button_click(5), lambda: on_button_click(7), lambda: on_button_click(9), lambda: on_button_click(11)])
 button_frame1.pack(pady=10)
-
 
 barrier_label = tk.Label(root, text="Choisissez le nombre de Joueurs (entre 2 et 4):")
 barrier_label.pack(pady=5)
-# Création des boutons 2 joueurs et 4 joueurs
+
 button_frame2 = tk.Frame(root)
-create_buttons_frame(button_frame2, ["2 joueurs", "4 joueurs"], [on_2_players_click, on_4_players_click])
+create_buttons_frame(button_frame2, ["2 joueurs", "4 joueurs"], [lambda: on_button_click(2), lambda: on_button_click(4)])
 button_frame2.pack(pady=10)
 
-# Création de la zone de texte pour choisir le nombre de barrières
 barrier_label = tk.Label(root, text="Choisissez le nombre de barrières (entre 4 et 40):")
 barrier_label.pack(pady=5)
 
 barrier_entry = tk.Entry(root)
 barrier_entry.pack(pady=5)
 
-# Création du bouton pour choisir le nombre de barrières
 barrier_button = tk.Button(root, text="Valider", command=show_barrier_dialog)
 barrier_button.pack(pady=5)
 
+reset_button = tk.Button(root, text="Réinitialiser", command=reset_game)
+reset_button.pack(pady=5)
+
+# Create a label to display the timer
+timer_label = tk.Label(root, text="00:00:00", font=("Arial", 24))
+timer_label.pack(pady=10)
+
+refresh_page()
 root.mainloop()
